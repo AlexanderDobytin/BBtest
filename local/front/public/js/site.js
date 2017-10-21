83d064168edb731f76348876061353f9e80e2105
Alexander Dobytin <dobytin@echo-company.ru>
2017-10-21 14:46:53 +0500

    ыу

--- local/front/public/js/site.js
+++ local/front/public/js/site.js
@@ -1,1 +1,35 @@
-let prop = 10;
+class item {
+    constructor() {
+        this.height()
+        $('.b-catalog-new').stick_in_parent()
+        $(window).resize(() => {
+            this.height()
+        })
+    }
+
+    height() {
+        $('.b-catalog-item__container').css({ 'height': $('.b-catalog-item__container').width() })
+        $('.b-catalog-new').css({ 'height': $('.b-catalog-new').width() })
+    }
+}
+
+
+class City{
+    constructor(){
+        
+        let that = this;
+        $('.b-catalog-new__submit').on('click',function(event){
+            event.preventDefault();
+            $.fancybox.open({
+                src  : '#hidden-content'})
+        })
+    }
+    PopUp(){
+        
+    }
+}
+$(document).ready(function () {
+    new item()
+    new City()
+    $("[data-fancybox]").fancybox();
+})
