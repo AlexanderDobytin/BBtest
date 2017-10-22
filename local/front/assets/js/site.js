a0f34631879a2364e91e7dc6971a1ee36e80cdb7
alexanderDobytin <sibsubtech@gmail.com>
2017-10-22 11:45:41 +0300

    all done

--- local/front/assets/js/site.js
+++ local/front/assets/js/site.js
@@ -16,20 +16,51 @@ class item {
 
 class City{
     constructor(){
-        
         let that = this;
         $('.b-catalog-new__submit').on('click',function(event){
             event.preventDefault();
             $.fancybox.open({
                 src  : '#hidden-content'})
         })
+        $('.b-popUp__form').on('submit',function(event){
+            event.preventDefault();
+            that.submit(this)
+        })
     }
-    PopUp(){
+    submit(item){
+        let data = $(item).serializeArray() ;
         
+               var newdata = [] ;
+               data.forEach(function(item,index){
+             
+                       newdata[item.name] = item.value                
+               })
+              this.request(newdata);
+    }
+    request(data){
+            let that = this
+        $.ajax({
+            url: '/place',
+            method:"POST",
+            data:{"city":data.city,"name":data.name,"link":data.link,},
+            success: function(answer){
+               that.add(answer);
+              
+            }
+          });
+    }
+    add(answer){
+            let item = `<div class="b-catalog-item col-xs-4">
+            <div style="background: url('`+answer.link+`') 50% center / cover no-repeat; height: 270px;" class="b-catalog-item__container"><a href="" class="b-catalog-item__textblock">
+                <div class="b-catalog-item__title">`+answer.name+`</div>
+                <div class="b-catalog-item__subtitle">`+answer.city+`</div></a></div>
+          </div>`;
+          $('.b-catalog__list').append(item);
+          $.fancybox.close( true );
     }
 }
 $(document).ready(function () {
     new item()
     new City()
-    $("[data-fancybox]").fancybox();
+  
 })
